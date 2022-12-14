9e72b3976527d05675ac037eedc91bb12f65c12d
Ben Tomasini <btomasini@activated.io>
2022-12-14 12:21:26 -0800

    updated static website with distribution

--- src/staticwebsite/staticwebsite.ts
+++ src/staticwebsite/staticwebsite.ts
@@ -1,9 +1,14 @@
 import { Construct } from "constructs";
 import * as aws from "@cdktf/provider-aws"
 import { Tags } from "../tags";
+import { DataAwsWafSubscribedRuleGroup } from "@cdktf/provider-aws/lib/data-aws-waf-subscribed-rule-group";
 
 interface StaticWebsiteProps {
-
+  viewerCertificate?: aws.cloudfrontDistribution.CloudfrontDistributionViewerCertificate;
+  customErrorResponses?: aws.cloudfrontDistribution.CloudfrontDistributionCustomErrorResponse[];
+  restrictions?: aws.cloudfrontDistribution.CloudfrontDistributionRestrictions;
+  webAclId?: string,
+  aliases?: string[],
   tags: Tags,
 }
 
@@ -59,12 +64,11 @@ class WebsiteBucket extends Construct {
 
 class StaticWebsite extends Construct {
 
+  public distribution: aws.cloudfrontDistribution.CloudfrontDistribution
+
   constructor(scope: Construct, id: string, props: StaticWebsiteProps) {
 
     super(scope, id)
-
-    const serverSideEncryption = {
-    };
 
     const sourceName = `${id}-source`;
     const logsName = `${id}-logs`;
@@ -90,6 +94,83 @@ class StaticWebsite extends Construct {
       acl: "private",
     })
 
+    const identity = new aws.cloudfrontOriginAccessIdentity.CloudfrontOriginAccessIdentity(this, "identity", {})
+
+    new aws.s3BucketPolicy.S3BucketPolicy(this, "bucketPolicy", {
+      bucket: sourceBucket.bucket.id,
+      policy: `{
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "1",
+                "Effect": "Allow",
+                "Principal": {
+                    "AWS": "${identity.iamArn}"
+                },
+                "Action": [
+                    "s3:GetObject"
+                ],
+                "Resource": "${sourceBucket.bucket.arn}/*"
+            }
+        ]
+    }
+      ` ,
+    })
+
+    const restrictions: aws.cloudfrontDistribution.CloudfrontDistributionRestrictions = props.restrictions ? props.restrictions : {
+      geoRestriction: {
+        restrictionType: "none" ,
+        locations: [],
+      }
+    }
+    
+    const viewerCertificate: aws.cloudfrontDistribution.CloudfrontDistributionViewerCertificate = props.viewerCertificate ? props.viewerCertificate : {
+      cloudfrontDefaultCertificate: true,
+    }
+
+    this.distribution = new aws.cloudfrontDistribution.CloudfrontDistribution(this, "distribution", {
+      webAclId: props.webAclId,
+      aliases: props.aliases,
+      restrictions: restrictions,
+      defaultCacheBehavior: {
+        allowedMethods: ['GET', 'HEAD'],
+        cachedMethods: ['GET', 'HEAD'],
+        compress: true,
+        forwardedValues: {
+          cookies: {
+            forward: 'none',
+          },
+          queryString: false,
+        },
+        targetOriginId: 'origin1',
+        viewerProtocolPolicy: 'redirect-to-https',
+      },
+      defaultRootObject: 'index.html',
+      customErrorResponse: props.customErrorResponses,
+      enabled: true,
+      httpVersion: 'http2',
+      isIpv6Enabled: true,
+      loggingConfig: {
+        includeCookies: false,
+        bucket: logsBucket.bucket.bucketRegionalDomainName,
+        prefix: 'cloudfront-logs',
+      },
+      origin: [
+        {
+          domainName: sourceBucket.bucket.bucketRegionalDomainName,
+          originId: 'origin1',
+          s3OriginConfig: {
+            originAccessIdentity: identity.cloudfrontAccessIdentityPath,
+          },
+        },
+      ],
+      priceClass: 'PriceClass_100',
+      viewerCertificate: viewerCertificate,
+      comment: `Website: ${id}`,
+      
+
+      tags: props.tags.withName(id).getTags(),
+    })
   }
 }
 
