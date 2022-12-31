130175ae7651d1cda66b8496c7ca809be2466043
Ben Tomasini <btomasini@activated.io>
2022-12-31 13:42:42 -0800

    deduplicating the dns records

--- src/certificate/verifiedcertificate.ts
+++ src/certificate/verifiedcertificate.ts
@@ -8,6 +8,7 @@ interface VerifiedCertificateProps {
   certificateProvider?: aws.provider.AwsProvider;
   tags: Tags;
   subjectAlternativeNames?: string[];
+  skipVerification?: boolean;
 }
 
 class VerifiedCertificate extends Construct {
@@ -24,38 +25,66 @@ class VerifiedCertificate extends Construct {
       validationMethod: 'DNS',
     });
 
-    new aws.route53Record.Route53Record(this, 'record', {
-      zoneId: props.zoneId,
-      name: this.certificate.domainValidationOptions.get(0).resourceRecordName,
-      type: this.certificate.domainValidationOptions.get(0).resourceRecordType,
-      ttl: 300,
-      records: [
-        this.certificate.domainValidationOptions.get(0).resourceRecordValue,
-      ],
-      lifecycle: {
-        createBeforeDestroy: false,
-      },
-    });
+    const created: {[key: string]: boolean} = {};
 
-    if (props.subjectAlternativeNames) {
-      for (let i = 0; i < props.subjectAlternativeNames.length; i++) {
-        const name = props.subjectAlternativeNames[i];
-        const index = i + 1;
-        new aws.route53Record.Route53Record(this, `recordAlt-${i}`, {
-          zoneId: props.zoneId,
-          name: this.certificate.domainValidationOptions.get(index)
-            .resourceRecordName,
-          type: this.certificate.domainValidationOptions.get(index)
-            .resourceRecordType,
-          ttl: 300,
-          records: [
-            this.certificate.domainValidationOptions.get(index)
-              .resourceRecordValue,
-          ],
-          lifecycle: {
-            createBeforeDestroy: false,
-          },
-        });
+    if (!props.skipVerification) {
+      new aws.route53Record.Route53Record(this, 'record', {
+        zoneId: props.zoneId,
+        name: this.certificate.domainValidationOptions.get(0)
+          .resourceRecordName,
+        type: this.certificate.domainValidationOptions.get(0)
+          .resourceRecordType,
+        ttl: 300,
+        records: [
+          this.certificate.domainValidationOptions.get(0).resourceRecordValue,
+        ],
+        lifecycle: {
+          createBeforeDestroy: false,
+        },
+
+        // deduplicate
+      });
+
+      created[
+        this.certificate.domainValidationOptions.get(0).resourceRecordName
+      ] = true;
+
+      if (props.subjectAlternativeNames) {
+        for (let i = 0; i < props.subjectAlternativeNames.length; i++) {
+          const index = i + 1;
+
+          if (
+            created[
+              this.certificate.domainValidationOptions.get(index)
+                .resourceRecordName
+            ]
+          ) {
+            // deduplicate
+            continue;
+          }
+
+          new aws.route53Record.Route53Record(this, `recordAlt-${i}`, {
+            zoneId: props.zoneId,
+            name: this.certificate.domainValidationOptions.get(index)
+              .resourceRecordName,
+            type: this.certificate.domainValidationOptions.get(index)
+              .resourceRecordType,
+            ttl: 300,
+            records: [
+              this.certificate.domainValidationOptions.get(index)
+                .resourceRecordValue,
+            ],
+            lifecycle: {
+              createBeforeDestroy: false,
+            },
+          });
+
+          created[
+            this.certificate.domainValidationOptions.get(
+              index
+            ).resourceRecordName
+          ] = true;
+        }
       }
     }
   }
